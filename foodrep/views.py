29b5b6260052cd36444007409c1613cc1c02da0a
mynameunname <dimamaga@gmail.com>
2018-05-28 13:49:18 +0300

    Dynamiqs views

--- foodrep/views.py
+++ foodrep/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Food
 
 def food_list(request):
-    return render(request, 'foodrep/food_list.html', {})
+    foods = Food.objects.all()
+    return render(request, 'foodrep/food_list.html', {'foods': foods})
