32df9a9059a30a92ee95e0d9459973b4c620a1f1
mynameunname <dimamaga@gmail.com>
2018-05-30 11:02:42 +0300

    Fixe

--- foodrep/models.py
+++ foodrep/models.py
@@ -27,16 +27,29 @@ class Food(models.Model):
         d2 = str(self.expire)
         d1 = datetime.strptime(str(datetime.now()), "%Y-%m-%d %H:%M:%S.%f")
         d2 = datetime.strptime(d2, "%Y-%m-%d")
-        return abs((d2 - d1).days)
+        return (d2 - d1).days
 
     def days_to_exp(self):
         d1 = datetime.strptime(str(self.created_date), "%Y-%m-%d")
         d2 = datetime.strptime(str(self.expire), "%Y-%m-%d")
-        return abs((d2 - d1).days)
+        return (d2 - d1).days
 
     def percent_to_exp(self):
-        result = 100/(self.days_to_exp()/self.days_to_exp_from_now())
-        return str(round(100 - result, 1)).replace(",",".")
+        if self.days_to_exp_from_now() > 0:
+            result = 100/(self.days_to_exp()/self.days_to_exp_from_now())
+            result = str(round(100 - result, 1)).replace(",",".")
+        else:
+            result = 100
+        return result
+
+    def type_of_result(self):
+        if self.days_to_exp_from_now() <= 0 or self.percent_to_exp() >= 80 :
+            type = "bg-danger"
+        elif self.percent_to_exp() > 0 and self.percent_to_exp() < 50:
+            type = "bg-success"
+        elif self.percent_to_exp() >= "50" and self.percent_to_exp() < 80:
+            type = "bg-warning"
+        return type
 
     def __str__(self):
         return self.title
